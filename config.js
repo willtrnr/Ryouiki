cbe5f260c47970d72311b892c0344c4b094fae7b
William Turner <willtur.will@gmail.com>
2013-04-03 18:17:49 -0400

    Page size config

--- config.js
+++ config.js
@@ -13,5 +13,6 @@ module.exports = {
   datadir   : process.env.OPENSHIFT_DATA_DIR || (__dirname + '/data'),
   thumbsize : 128,
   maxsize   : '5mb',
-  replies   : 2
+  replies   : 2,
+  pagesize  : 15
 };
